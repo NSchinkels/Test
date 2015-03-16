c6fb9137b2cebcff26cc69c19947bc424bf720b7
CodingSession <n.schinkels@googlemail.com>
2015-03-16 19:48:39 +0100

    testing...
    Signed-off-by: CodingSession

--- src/Chess.java
+++ src/Chess.java
@@ -1,7 +1,14 @@
 
 public class Chess {
+	public Chess(){
+		ersteMetohde();
+	}
+	
 	public void ersteMetohde(){
 		//in ecplise danach geschrieben
 		System.out.println("Geschrieben nicht in ecplise");
+	}
+	public static void main(String[] args){
+		new Chess();
 	}
 }
